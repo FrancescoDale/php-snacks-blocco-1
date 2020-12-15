fefeb7fb02060b9fb073ac77d9885b316b67507f
FrancescoDale <fd060862@gmail.com>
2020-12-15 15:25:12 +0100

    uso is_numeric() per verificare che l'età sia un numero.

--- snack2/index.php
+++ snack2/index.php
@@ -22,7 +22,7 @@ $mail = $_GET["mail"];
             <h1 class="text-center text-capitalize mt-2 mb-4">status accesso : </h1>
 
             <p class="text-capitalize text-center">
-                <?php if (strlen($name) > 3 && strpos($mail, '@') && strpos($mail, '.')) {
+                <?php if (strlen($name) > 3 && strpos($mail, '@') && strpos($mail, '.') && is_numeric($age)) {
                     echo "accesso riuscito";
                 } else {
                     echo "accesso negato";
