eab56353e84fa3e0b489eb77925abe344195591f
FrancescoDale <fd060862@gmail.com>
2020-12-15 14:57:21 +0100

    inizio snack 2

---
+++ snack2/index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>PHP snack 2</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="text-center text-capitalize mt-2 mb-4">status accesso : </h1>
+
+
+        </div>
+
+
+    </body>
+</html>
